bb95e8077f3c3876a364508c5ea83bf05986e305
TheOnis <onisamuelmichael@gmail.com>
2022-12-08 14:26:23 +0100

    SignUp Popup screen done

---
+++ pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupModal from '../components/Modals/SignupModal/Index'
+
+const Signup = () => {
+  return (
+    <div>
+        <SignupModal/>
+    </div>
+  )
+}
+
+export default Signup
