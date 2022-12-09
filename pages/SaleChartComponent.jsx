fc9bbe14989c9a4c7fcb031c359d91d44417119b
TheOnis <onisamuelmichael@gmail.com>
2022-12-09 15:09:09 +0100

    Done with the Sales Chart Component in the Profile Page

---
+++ pages/SaleChartComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SaleChart from '../components/BarChart/Index'
+
+const SaleChartComponent = () => {
+  return (
+    <div>
+        <SaleChart />
+    </div>
+  )
+}
+
+export default SaleChartComponent
