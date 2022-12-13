538ccc4f201ac0fe14da1303746a7b8b5771af29
Abiodun Emmanuel <abiodunemma0394@gmail.com>
2022-12-13 15:42:09 +0100

    added new component

--- components/Beatcomponent/Index.jsx
+++ components/Beatcomponent/Index.jsx
@@ -2,35 +2,65 @@ import Image from 'next/image';
 import style from './beatcomponent.module.scss';
 import cart from '../../assets/cart.svg';
 import cartimg from '../../assets/cartimg.svg';
-import {FaPlay} from 'react-icons/fa';
-import {FaPause} from 'react-icons/fa';
+import { FaPlay } from 'react-icons/fa';
+import { FaPause } from 'react-icons/fa';
 import React, { useState, useRef, useEffect } from 'react';
 
 function Beatcomponent() {
-    // state
+  // state
   const [isPlaying, setIsPlaying] = useState(false);
-  const [duration, setDuration] = useState (0);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
 
+  //   references
+  const audioPlayer = useRef(); //reference our audio component
+  const progressBar = useRef(); //reference our progress bar
+  const animationRef = useRef(); //reference the animation
 
-//   references
-    const audioPlayer = useRef();   //reference our audio component
+  useEffect(() => {
+    const seconds = Math.floor(audioPlayer.current.duration);
+    setDuration(seconds);
+    progressBar.current.max = seconds;
+  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
 
-
-    useEffect( () => {
-
-    }, [audioPlayer?.current?.loadedmetadata]);
-
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${minutes}` : `${minutes}`;
+    return `${returnedMinutes} : ${returnedSeconds}`;
+  };
 
   const togglePlayPause = () => {
     const prevValue = isPlaying;
     setIsPlaying(!prevValue);
     if (!prevValue) {
-        audioPlayer.current.play();
-    } else{
-        audioPlayer.current.pause();
+      audioPlayer.current.play();
+      animationRef.current = requestAnimationFrame(whilePlaying);
+    } else {
+      audioPlayer.current.pause();
+      cancelAnimationFrame(animationRef.current);
     }
+  };
 
-  }
+  const whilePlaying = () => {
+    progressBar.current.value = audioPlayer.current.currentTime;
+    changePlayerCurrentTime();
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  };
+
+  const changeRange = () => {
+    audioPlayer.current.currentTime = progressBar.current.value;
+    changePlayerCurrentTime();
+  };
+
+  const changePlayerCurrentTime = () => {
+    progressBar.current.style.setProperty(
+      '--seek-before-width',
+      `${(progressBar.current.value / duration) * 100}%`
+    );
+    setCurrentTime(progressBar.current.value);
+  };
 
   return (
     <div>
@@ -62,31 +92,39 @@ function Beatcomponent() {
       </div>
 
       {/* The Media component */}
-      <div className={style.audioplayer}>
-        <audio ref ={audioPlayer} src="https://tooxclusive.com/wp-content/uploads/2014/02/Davido-Aye-_-tooXclusive.com_1.mp3"
-          preload="metadata"
-        >
-          {' '}
-        </audio>
+      <div className={style.audiocontainer}>
+        <div className={style.audioPlayer}>
+          <audio
+            ref={audioPlayer}
+            src="https://tooxclusive.com/wp-content/uploads/2014/02/Davido-Aye-_-tooXclusive.com_1.mp3"
+            preload="metadata"
+          >
+            {' '}
+          </audio>
 
-        <button onClick={togglePlayPause} className={style.playPause}>
-            { isPlaying ? <FaPause /> : <FaPlay className={style.play} /> }
-        </button>
+          <button onClick={togglePlayPause} className={style.playPause}>
+            {isPlaying ? <FaPause /> : <FaPlay className={style.play} />}
+          </button>
 
-        {/* current time */}
-        <div className={style.currentTime}>0:00</div>
+          {/* current time */}
+          <div className={style.currentTime}>{calculateTime(currentTime)}</div>
 
+          {/* progress bar */}
+          <div>
+            <input
+              type="range"
+              className={style.progressBar}
+              defaultValue="0"
+              ref={progressBar}
+              onChange={changeRange}
+            />
+          </div>
 
-
-        {/* progress bar */}
-        <div>
-            <input type="range" className={style.progressBar}/>
+          {/* duration  */}
+          <div className={style.duration}>
+            {duration && !isNaN(duration) && calculateTime(duration)}
+          </div>
         </div>
-        
-
-
-        {/* duration  */}
-        <div className={style.duration}>{duration}</div>
       </div>
     </div>
   );
