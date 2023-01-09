fee9ab323461605d07a904c5d4cbeb0607d7c873
Odiri Ighogboja <odizeeplatform@gmail.com>
2023-01-09 08:03:00 +0100

    desktop veiw for homepage

--- components/Buyingoption/BuyOptionBox.jsx
+++ components/Buyingoption/BuyOptionBox.jsx
@@ -3,9 +3,9 @@ import styles from './buyingoption.module.scss';
 
 function BuyOptionBox() {
   return OptionData.map((option) => (
-    <div className={styles.radiobox}>
+    <div className={styles.radiobox} key={option.id}>
       <label htmlFor="buyoption">
-        <input type="radio" name="radio" checked id="buyoption" />
+        <input type="radio" name="radio" id="buyoption" />
         <div className={`${styles.radiobox__item} ${styles.box}`}>
           <h3>{option.name}</h3>
           <h4>NGN {option.price}</h4>
--- components/Buyingoption/OptionData.js
+++ components/Buyingoption/OptionData.js
@@ -1,25 +1,30 @@
 export const OptionData = [
   {
+    id: 1,
     name: 'Basic WAV LEASE',
     price: 15000,
     format: 'MP3 AND WAV',
   },
   {
+    id: 2,
     name: 'Basic WAV LEASE',
     price: 15000,
     format: 'MP3 AND WAV',
   },
   {
+    id: 3,
     name: 'Basic WAV LEASE',
     price: 15000,
     format: 'MP3 AND WAV',
   },
   {
+    id: 4,
     name: 'Basic WAV LEASE',
     price: 15000,
     format: 'MP3 AND WAV',
   },
   {
+    id: 5,
     name: 'Basic WAV LEASE',
     price: 15000,
     format: 'MP3 AND WAV',
