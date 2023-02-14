d85732a27f85cf8fa580f7446791033fbb4a4d77
Oni Michael <onisamuelmichael@gmail.com>
2023-02-14 16:21:00 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import Page from "../components/Page";
 import "../styles/globals.css";
 import "@fontsource/open-sans";
-import { ApiProvider } from "@reduxjs/toolkit/query/react";
-import { KultureApi } from "./features/apiSlice";
+//import { ApiProvider } from "@reduxjs/toolkit/query/react";
+//import { KultureApi } from "./features/apiSlice";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApiProvider api={KultureApi}>
+    //<ApiProvider api={KultureApi}>
       <Component {...pageProps} />;
-    </ApiProvider>
+   // </ApiProvider>
   );
 }
 
