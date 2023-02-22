3d73379d3aaf5d8c5eaa6cf77ae17a5508152fcd
iamdonemmy <abiodunemma0394@gmail.com>
2023-02-22 02:07:49 +0100

    endpoint created

--- state/store/index.js
+++ state/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { UserApi } from "../services/UserApi";
+import { ForgetPasswordApi } from "../services/ForgetPasswordApi";
 import globalReducer from "../../state";
 
 export const store = configureStore({
   reducer: {
     global: globalReducer,
     [UserApi.reducerPath]: UserApi.reducer,
+    [ForgetPasswordApi.reducerPath]: ForgetPasswordApi.reducer,
   },
   middleware: (getDefault) => getDefault().concat(UserApi.middleware),
 });
