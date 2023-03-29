e7cc78400ece99722e3307033c7caf49de44dfbd
TheOnis <onisamuelmichael@gmail.com>
2023-03-29 19:16:23 +0100

    beatcard dummylist created

--- components/BeatCard/BeatCard.jsx
+++ components/BeatCard/BeatCard.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import style from "./beatCard.module.scss";
+import { cardList } from "../Utils/Index";
+
 import miriam from "../../assets/miriam.svg";
 import miriam2 from "../../assets/miriam2.svg";
 import wintspread from "../../assets/wintspread.svg";
@@ -37,71 +39,13 @@ function BeatCard() {
       {/* First Card....Notes its hard coded */}
       {/* Will create a dummy list and map through later */}
       <div className={style.containerbeatcard1}>
-        <div className={style.card_header}>
-          <div>
-            <Image src={miriam} alt='image' className={style.imagebeatcard} />
-          </div>
-          <h3> Miriam </h3>
-          <p>@ohuly </p>
-          <div className={style.make_circle}></div>
-          <p> 2 Hours Ago</p>
-        </div>
-
-        <div className={style.card_wrap}>
-          <div className={style.wrap_image}>
-            <div className={style.image_snow}>
-              <Image src={wintspread} alt='Snow' className={style.snow} />
-            </div>
-            <div className={style.hideme}>
-              <Image src={videocircle} alt='videocircle' />
-            </div>
-          </div>
-          <div className={style.producer_content}>
-            <div className={style.producer_label}>
-              <div className={style.hidenow}>
-                <Image src={videocircle} alt='videocircle' />
-              </div>{" "}
-              <div>
-                <h4>Wintspread cabasky</h4>
-              </div>
-            </div>
-            <div className={style.beat_text}>
-              <p>
-                Beat by Miriam @ohuly
-                <br />
-                released on 23 November 2022
-              </p>
-            </div>
-
-            <div className={style.footerwrapper}>
-              <p className={style.footerwrappertext}>NGN 90,0000 </p>{" "}
-              <button className={style.btn_footer} type='submit'>
-                <Link href='/review'>BUY NOW</Link>
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className={style.downicons}>
-          <div className={style.footericontext1}>
-            <div className={style.footericon1}>
-              <Image src={share} alt='image' className={style.foot} />
-            </div>
-
-            <div className={style.footertext1}>
-              <p>share</p>
-            </div>
-          </div>
-
-          <div className={style.footericontext1}>
-            <div className={style.footericon1}>
-              <Image src={save} alt='image' className={style.foot} />
-            </div>
-            <div className={style.footertext1}>
-              <p>save for later</p>
-            </div>
-          </div>
-        </div>
+      {
+        cardList.map((card, index) => (
+          <>
+          
+          </>
+        ))}
+       
       </div>
       {/* -----------------------------------------------------------------------------
       --------------------------------------------------------------------------------- */}
