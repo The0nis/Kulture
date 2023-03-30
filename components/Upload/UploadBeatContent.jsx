da2529f96cdedfa68750b54682d0489b0da5bf27
iamdonemmy <abiodunemma0394@gmail.com>
2023-03-30 12:30:16 +0100

    Upload Pages Animation

--- components/Upload/UploadBeatContent.jsx
+++ components/Upload/UploadBeatContent.jsx
@@ -5,6 +5,7 @@ import cloud from "../../assets/cloud.svg";
 import Page from "../Page";
 import { useState } from "react";
 import { navigateBack } from "../../util";
+import { motion } from "framer-motion";
 
 function UploadBeatContent() {
   //declaring state for the input files
@@ -26,35 +27,63 @@ function UploadBeatContent() {
     }
   };
 
+  // Animation declaration
+  const easing = [0.6, -0.05, 0.01, 0.99];
+
+  const fadeInUp = {
+    initial: {
+      y: 60,
+      opacity: 0,
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.6,
+        ease: easing,
+      },
+    },
+  };
+
+  const stagger = {
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
   return (
-    <div>
+    <motion.div exit={{ opacity: 0 }} initial="initial" animate="animate">
       <Page>
-        <div className={style.cloudtextdiv}>
-          <p className={style.cloudtext}>Upload your beats to Kulture</p>
-        </div>
-        <label>
-          <div className={style.cloudcontainer}>
-            {/* if the image have not been selected display this */}
-            {!valid && (
-              <div className={style.wrap}>
-                <div className={style.cloud_image}>
-                  <Image src={cloud} alt='musicapp' />
-                </div>
+        <motion.div variants={stagger}>
+          <motion.div variants={fadeInUp}>
+            <div className={style.cloudtextdiv}>
+              <p className={style.cloudtext}>Upload your beats to Kulture</p>
+            </div>
+            <label>
+              <div className={style.cloudcontainer}>
+                {/* if the image have not been selected display this */}
+                {!valid && (
+                  <div className={style.wrap}>
+                    <div className={style.cloud_image}>
+                      <Image src={cloud} alt="musicapp" />
+                    </div>
 
-                <div className={style.cloudtextdiv}>
-                  <p className={style.cloudtext1}>
-                    Browse for files or drag and drop them here
-                  </p>
-                </div>
+                    <div className={style.cloudtextdiv}>
+                      <p className={style.cloudtext1}>
+                        Browse for files or drag and drop them here
+                      </p>
+                    </div>
 
-                <div className={style.cloudtextdiv}>
-                  <p className={style.cloudtext2}>Max. File Size: 500 MB</p>
-                </div>
-              </div>
-            )}
+                    <div className={style.cloudtextdiv}>
+                      <p className={style.cloudtext2}>Max. File Size: 500 MB</p>
+                    </div>
+                  </div>
+                )}
 
-            {/* if a valid file have  been selected display  this */}
-            {/* {valid && (
+                {/* if a valid file have  been selected display  this */}
+                {/* {valid && (
               <div
                 className={style.b_img}
                 style={{
@@ -65,37 +94,39 @@ function UploadBeatContent() {
               ></div>
             )} */}
 
-            {valid && (
-              <div className={style.musicContainer}>
-                <div className={style.musicInfo}>
-                  <p className={style.musicName}>{file.name}</p>
-                  {file.image && (
-                    <img src={file.image} className={style.musicImage} />
-                  )}
-                </div>
-                <audio
-                  controls
-                  className={style.audioPlayer}
-                  src={file}
-                ></audio>
+                {valid && (
+                  <div className={style.musicContainer}>
+                    <div className={style.musicInfo}>
+                      <p className={style.musicName}>{file.name}</p>
+                      {file.image && (
+                        <img src={file.image} className={style.musicImage} />
+                      )}
+                    </div>
+                    <audio
+                      controls
+                      className={style.audioPlayer}
+                      src={file}
+                    ></audio>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-          <div className={style.input_field}>
-            <input type='file' onChange={handleChange} accept='audio/*' />
-          </div>
-        </label>
+              <div className={style.input_field}>
+                <input type="file" onChange={handleChange} accept="audio/*" />
+              </div>
+            </label>
 
-        <div className={style.upload_btn}>
-          <button className={style.back_btn} onClick={navigateBack}>
-            Back
-          </button>
-          <button className={style.next_btn}>
-            <Link href='/new-upload/upload-form'>Next</Link>
-          </button>
-        </div>
+            <div className={style.upload_btn}>
+              <button className={style.back_btn} onClick={navigateBack}>
+                Back
+              </button>
+              <button className={style.next_btn}>
+                <Link href="/new-upload/upload-form">Next</Link>
+              </button>
+            </div>
+          </motion.div>
+        </motion.div>
       </Page>
-    </div>
+    </motion.div>
   );
 }
 
