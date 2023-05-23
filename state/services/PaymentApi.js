7ee59f4a8e5efc063775520ce1b71ce8be13c310
kazeem <aigorokazeem90@gmail.com>
2023-05-23 09:12:01 +0100

    Modified the baseUrl for the Api

--- state/services/PaymentApi.js
+++ state/services/PaymentApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const PaymentApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://54.236.30.189:9000/api/" }),
   reducerPath: "PaymentApi",
   endpoints: (builder) => ({
     callback: builder.mutation({
--- state/services/UserApi.js
+++ state/services/UserApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const UserApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://54.236.30.189:9000/api/" }),
   reducerPath: "userApi",
   tagTypes: ["User"],
   endpoints: (builder) => ({
